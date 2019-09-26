59919451a971adc329aad23c34aca2ae33a46cf9
msc0001 <singh.m0710@gmail.com>
2019-09-26 10:09:50 +0530

    added printArray

---
+++ PrintArray.java
@@ -0,0 +1,9 @@
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2, 3, 4, 5, 6};
+        for (int var : arr) {
+            System.out.print(var + ',');
+        }
+        System.out.println();
+    }
+}
